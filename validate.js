4a37d7170320c3e005df4164ecf5e15fb8b6025b
Kanyapak <modemkanyapak@gmail.com>
2025-03-05 16:36:11 +0700

    Revert "Bug fix#2 - adding student id validation (id must be composed of numbers between 0 and 9)"
    This reverts commit 2b9a6f5c144390c314133115ed2c7cdfc5d8c68a.

--- validate.js
+++ validate.js
@@ -1,7 +1,6 @@
 function checkSID() {
   let sid = (document.getElementById("sid").value).trim();
-  // ตรวจสอบว่าเป็นตัวเลขล้วนและมีความยาว 10 ตัว
-  if (!isNaN(sid) && sid.length === 10) {
+  if (sid.length == 10) {
     return true;
   } else {
     return false;
